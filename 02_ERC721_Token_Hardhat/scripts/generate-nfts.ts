fdbc2e046199018fed08cac02c335605d1f68485
zeeshanhanif <zeeshanhanif@gmail.com>
2021-10-07 00:54:32 +0500

    Added contract address

--- 02_ERC721_Token_Hardhat/scripts/generate-nfts.ts
+++ 02_ERC721_Token_Hardhat/scripts/generate-nfts.ts
@@ -3,7 +3,7 @@ import { NewNFT__factory } from "../typechain";
 
 async function main() {
     
-    const newNFTAddress = "<NFT Address after contract deployment>";
+    const newNFTAddress = "0x0E41629d2e65B05807C59D4722697Cc539617BB9";
     const NewNFT:NewNFT__factory = await ethers.getContractFactory("NewNFT");
     const newnft = await NewNFT.attach(newNFTAddress);
 
