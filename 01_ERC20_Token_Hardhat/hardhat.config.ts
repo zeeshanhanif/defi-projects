7dedcf2025ae85abeb62ac284faa86a28453a30d
zeeshanhanif <zeeshanhanif@gmail.com>
2021-10-06 17:32:54 +0500

    Project 1 - Added deployment to test networks

--- 01_ERC20_Token_Hardhat/hardhat.config.ts
+++ 01_ERC20_Token_Hardhat/hardhat.config.ts
@@ -2,6 +2,18 @@ import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const INFURA_KEY = process.env.INFURA_KEY;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,4 +33,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 export default {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
+    }
+  }
 };
