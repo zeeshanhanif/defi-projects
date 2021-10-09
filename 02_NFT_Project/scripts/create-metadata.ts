ab5c889434dc54a36ec5d757f4d1e5c08d86f0c2
zeeshanhanif <zeeshanhanif@gmail.com>
2021-10-10 02:10:14 +0500

    Script to generate metadata

---
+++ 02_NFT_Project/scripts/create-metadata.ts
@@ -0,0 +1,42 @@
+
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { ethers, run } from 'hardhat';
+import { ShapeNFT, ShapeNFT__factory } from '../typechain';
+import { metadata_template, shapes } from './data';
+
+async function main() {
+    if(!existsSync("metadata")){
+        mkdirSync("metadata");
+    }
+    for(let i=0;i<shapes.length;i++){
+        let shapeMetadata = metadata_template;
+        //console.log("Shapes = ",shapes[i]);
+        shapeMetadata.name = shapes[i].name;
+        shapeMetadata.description= `${shapes[i].name} Shape with ${shapes[i].color} color`;
+        
+        // According to this documentation https://docs.opensea.io/docs/metadata-standards#ipfs-and-arweave-uris
+        // We will provide image like ipfs://<hash>
+        // Replace this hash with your hash
+        shapeMetadata.image=`ipfs://QmfFDBWoVQc1X5Lzdqv9XsbzdrHtvp4uAHNjqJq1gPqcNV/${shapes[i].imageName}`;
+        shapeMetadata.attributes[0].value = shapes[i].name;
+        shapeMetadata.attributes[1].value = shapes[i].color;
+
+        console.log(shapeMetadata);
+        
+        let filename = "metadata/"+shapes[i].imageName.toLowerCase().replace(/\s/g, '-').replace("png","json");
+        console.log("filename = ",filename);
+        console.log("==============");
+        let data = JSON.stringify(shapeMetadata);
+        writeFileSync(filename,data);
+        
+    }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ 02_NFT_Project/scripts/data.ts
@@ -0,0 +1,78 @@
+export const shapes = [
+    {
+        name:"Triangle",
+        color: "Light Yellow",
+        imageName: "01_triangle_light_yellow.png"
+    },
+    {
+        name:"Pentagon",
+        color: "Light Yellow",
+        imageName: "02_pentagon_light_yellow.png"
+    },
+    {
+        name:"Hexagon",
+        color: "Light Yellow",
+        imageName: "03_hexagon_light_yellow.png"
+    },
+    {
+        name:"Diamond",
+        color: "Light Yellow",
+        imageName: "04_diamond_light_yellow.png"
+    },
+    {
+        name:"Arc",
+        color: "Light Yellow",
+        imageName: "05_arc_light_yellow.png"
+    },
+    {
+        name:"Star: 4 point",
+        color: "Light Yellow",
+        imageName: "06_star_4point_light_yellow.png"
+    },
+    {
+        name:"Triangle",
+        color: "Turquoise",
+        imageName: "07_triangle_turquoise.png"
+    },
+    {
+        name:"Pentagon",
+        color: "Turquoise",
+        imageName: "08_pentagon_turquoise.png"
+    },
+    {
+        name:"Hexagon",
+        color: "Turquoise",
+        imageName: "09_hexagon_turquoise.png"
+    },
+    {
+        name:"Diamond",
+        color: "Turquoise",
+        imageName: "10_diamond_turquoise.png"
+    },
+    {
+        name:"Arc",
+        color: "Turquoise",
+        imageName: "11_arc_turquoise.png"
+    },
+    {
+        name:"Star: 4 point",
+        color: "Turquoise",
+        imageName: "12_star_4point_turquoise.png"
+    }    
+];
+
+export const metadata_template = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [
+        {
+            "trait_type": "Name",
+            "value": ""
+        },
+        {
+            "trait_type": "Color",
+            "value": ""
+        },
+    ]
+}
