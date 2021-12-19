b08adad53c47408a2ecf5ce927ebadbe324f3e32
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-19 23:41:07 +0500

    Added basic implementation of ERC20 token that will be used for stacking

---
+++ 04_ERC20_Stacking/contracts/SToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SToken is ERC20 {
+    
+    constructor() ERC20("S Token","STK") {
+        _mint(msg.sender,1000 * 10 ** decimals());
+    }
+}
