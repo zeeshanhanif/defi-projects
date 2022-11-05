3dcf7b5aabc23928929ef81f7dfb5c2f807c84ef
zeeshanhanif <zeeshanhanif@gmail.com>
2022-11-05 23:36:38 +0500

    Updated package.json and MainCreator

--- 09_DelegateDeploy/contracts/MainCreator.sol
+++ 09_DelegateDeploy/contracts/MainCreator.sol
@@ -4,6 +4,7 @@ pragma solidity ^ 0.8.4;
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "hardhat/console.sol";
+import "./DemoNFT.sol";
 
 contract MainCreator is OwnableUpgradeable, UUPSUpgradeable {
 
@@ -27,7 +28,10 @@ contract MainCreator is OwnableUpgradeable, UUPSUpgradeable {
 
     function setData(uint256 _age) public {
         console.log("MainCreator::setData - _age = ", _age);
+        console.log("MainCreator::setData - msg.sender = ", msg.sender);
+        DemoNFT dm = new DemoNFT("AAA", "BBB");
         age = _age;
+        console.log("MainCreator::setData - after contract creation = ", address(dm));
     }
 
     function getData() public view returns(uint256) {
