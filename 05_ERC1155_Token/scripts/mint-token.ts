da5dcc9d197314f19ed35a784db0986e446ea2f9
zeeshanhanif <zeeshanhanif@gmail.com>
2023-01-15 18:11:26 +0500

    Updated script to mint tokens

--- 05_ERC1155_Token/scripts/mint-token.ts
+++ 05_ERC1155_Token/scripts/mint-token.ts
@@ -3,16 +3,20 @@ import { ethers, run } from 'hardhat';
 import { MultiTokenNFT, MultiTokenNFT__factory } from '../typechain';
 
 async function main() {
+  const [owner, addr1] = await ethers.getSigners();
 
   //https://game.example/api/item/{id}.json
   const MultiTokenNFT:MultiTokenNFT__factory = await ethers.getContractFactory("MultiTokenNFT");
-  const multiTokenNFT:MultiTokenNFT = await MultiTokenNFT.attach("0xBE4f86C27Ec9dbF11E3772ACc06Af42df4078B2E");
+  const multiTokenNFT:MultiTokenNFT = await MultiTokenNFT.attach("0x0F4763d3652aFB7d061909C5080ee9323b292540");
   
   console.log("MultiTokenNFT Address:", multiTokenNFT.address);
 
   const tokenId = BigNumber.from("2");
   const numberOfTokens = BigNumber.from("4");
-  const txt:ContractTransaction = await multiTokenNFT.mintToken(tokenId, numberOfTokens);
+  const txt:ContractTransaction = await multiTokenNFT.mintToken(owner.address, tokenId, numberOfTokens);
+  // logesh: 0x07C920eA4A1aa50c8bE40c910d7c4981D135272B
+  // Adam: 0xa0cb079d354b66188f533a919d1c58cd67afe398
+
   console.log("Transaction Hash = ",txt.hash);
 
   const receipt:ContractReceipt = await txt.wait();
