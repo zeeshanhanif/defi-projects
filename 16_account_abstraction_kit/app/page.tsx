bf35ccffceeb0c7939a006f51781b5c1626a4a31
zeeshanhanif <zeeshanhanif@gmail.com>
2023-08-08 21:51:08 +0500

    Updated main page to display Mint button

--- 16_account_abstraction_kit/app/page.tsx
+++ 16_account_abstraction_kit/app/page.tsx
@@ -3,11 +3,13 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import ConnectBtn from './components/connectBtn'
 import { useAccount } from 'wagmi'
+import useSampleNFT from "./hooks/useSampleNFT";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const {address, isConnected, isReconnecting, isDisconnected, connector: activeConnector} = useAccount();
+  const { mintPass, getTotalSupply } = useSampleNFT();
   return (
     <div >
       Zero Dev App Login with Rainbow Kit
@@ -20,6 +22,13 @@ export default function Home() {
         isReconnecting: {isReconnecting?"Yes": "No"}<br/>
         isDisconnected: {isDisconnected?"Yes": "No"}<br/>
         Connected To: {activeConnector?.name}<br/>
+      </div>
+      <div>
+        <button className='w-60 bg-slate-800 ml-10 mt-10 text-white'
+          onClick={()=>{
+            mintPass()
+          }}
+        >Mint</button>
       </div>
     </div>
   )
