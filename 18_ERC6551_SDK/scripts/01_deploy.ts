d0ab4343ec90bb940d88a0d954865d88845a4df3
zeeshanhanif <zeeshanhanif@gmail.com>
2023-09-30 23:04:01 +0500

    Added script to deploy NFT

---
+++ 18_ERC6551_SDK/scripts/01_deploy.ts
@@ -0,0 +1,23 @@
+import { ethers, network } from "hardhat";
+import { DemoNFT, DemoNFT__factory } from "../typechain-types";
+
+async function main() {
+
+  const [owner, addr1] = await ethers.getSigners();
+  console.log("Network = ",network.name);
+
+  console.log("add = ", owner.address);
+
+  const DemoNFT:DemoNFT__factory = await ethers.getContractFactory("DemoNFT");
+  const demoNFT:DemoNFT = await DemoNFT.deploy("DemoNFT3", "DFT3");
+  await demoNFT.deployed();
+
+  console.log("DemoNFT deployed to:", demoNFT.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
