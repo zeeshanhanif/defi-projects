2926b4a757f2269dc4aa17745fd62d14f19f7194
zeeshanhanif <zeeshanhanif@gmail.com>
2023-10-28 22:32:37 +0500

    Added Main component to display NFT List

---
+++ 19_erc6551_web/app/components/Main.tsx
@@ -0,0 +1,12 @@
+import NFTList from "./NFTList";
+
+function Main() {
+
+    return (
+        <div className="bg-red-400 main-sec main-sec-lg">
+            <NFTList />
+        </div>
+    );
+}
+
+export default Main;
