fec255a8f7001b493f76bd2f956deac9be5c25e9
zeeshanhanif <zeeshanhanif@gmail.com>
2023-11-08 23:42:29 +0500

    Updated tokenbound to include transfer NFT functions

--- 19_erc6551_web/app/utils/tokenbound.ts
+++ 19_erc6551_web/app/utils/tokenbound.ts
@@ -1,4 +1,7 @@
 import { TokenboundClient} from "@tokenbound/sdk";
+import { erc721ABI, useWalletClient } from "wagmi";
+import {  readContract, writeContract, waitForTransaction } from '@wagmi/core'
+import { toast } from "react-toastify";
 
 export const getTokenboundClient = (chainId: number) => {
 
@@ -29,9 +32,7 @@ export const getIsAccountDeployed = async (walletClient:any, chainId: number, ac
 }
 
 export const deployAccount = async (walletClient:any, chainId: number, contractAddress:string, tokenId:string ):Promise<string> =>{
-    console.log("test = ", chainId);
     const tokenboundClient = new TokenboundClient({ walletClient, chainId });
-    console.log("token bound client = ", tokenboundClient); 
     
     const account = await tokenboundClient.createAccount({
         tokenContract: contractAddress as `0x${string}`,
@@ -39,4 +40,53 @@ export const deployAccount = async (walletClient:any, chainId: number, contractA
     })
     
     return String(account);
+}
+
+export const transferNFTFromTBA = async(walletClient:any, chainId: number | undefined, to:string | undefined, accountAddress:string | undefined, contractAddress:string | undefined, tokenId:string| undefined) => {
+    if(chainId && chainId>0 && to && tokenId) {
+        const tokenboundClient = new TokenboundClient({ walletClient, chainId });
+
+        const transferNFT = await tokenboundClient.transferNFT({
+            account: accountAddress as `0x${string}`,
+            tokenType: "ERC721",
+            tokenContract: contractAddress  as `0x${string}`,
+            tokenId: tokenId,
+            recipientAddress: to as `0x${string}`,
+        });
+        console.log("transferNFT = ",transferNFT);
+    }
+}
+
+export const transferNFTFromWallet = async( to: string | undefined, from: `0x${string}` | undefined, contractAddress:string | undefined, tokenId:string| undefined) => {
+    if(to && tokenId && contractAddress && from) {
+
+        const { hash } = await writeContract({
+                address: contractAddress as `0x${string}`,
+                abi: erc721ABI,
+                functionName: "safeTransferFrom",
+                args:[from, to as `0x${string}`,  BigInt(tokenId)]
+        })
+
+        console.log("Transaction Hash safeTransferFrom = ",hash);
+        toast.info("Transaction Hash: "+hash, {
+            position: toast.POSITION.TOP_CENTER
+        });
+        const data = await waitForTransaction({
+            hash: hash,
+        })
+        console.log("Wait Result = ",data);
+        console.log("Transfer Done");
+        if(data.status == "success") {
+            toast.success("Transfer Done", {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+        else {
+            toast.success("Unknown Error", {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+
+        
+    }
 }
