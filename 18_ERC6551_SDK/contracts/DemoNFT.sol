64832f2e7e312c2aa0b90908fa6a38030c130ffc
zeeshanhanif <zeeshanhanif@gmail.com>
2023-11-24 23:20:20 +0500

    Updated NFT contract

--- 18_ERC6551_SDK/contracts/DemoNFT.sol
+++ 18_ERC6551_SDK/contracts/DemoNFT.sol
@@ -10,9 +10,10 @@ contract DemoNFT is ERC721, Ownable {
     using Strings for uint256;
 
     string public defaultURI = "ipfs://QmXsMLpKjznF3z1KsVm5tNs3E94vj4BFAyAHvD5RTWgQ1J";
-    string private baseURI;
+    string private baseURI; //"https://coral-wasteful-lion-960.mypinata.cloud/ipfs/QmXFnSJYkichQkF2dxuFAvrYaJ4P8hveAifb26auBZE7bC/";
     uint256 public totalSupply;
     uint256 public price = 0.07 ether;
+    uint256 public constant MAX_SUPPLY = 50;
 
     event Minted(address sender, address to);
 
@@ -24,13 +25,23 @@ contract DemoNFT is ERC721, Ownable {
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(),".json")) : defaultURI;
     }
 
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string calldata _baseuri) public onlyOwner {
+		baseURI = _baseuri;
+	}
+
     function mint(address _to) public {
-        _safeMint(_to, totalSupply++);
+        require(totalSupply + 1 <= MAX_SUPPLY, "Minting would exceed max supply");
+        _safeMint(_to, ++totalSupply);
         emit Minted(msg.sender, _to);
     }
 
     function mint2() public payable {
         require(price == msg.value, "Incorrect Funds Sent" );
+        require(totalSupply + 1 <= MAX_SUPPLY, "Minting would exceed max supply");
         _safeMint(msg.sender, totalSupply++);
         emit Minted(msg.sender, msg.sender);
     }
